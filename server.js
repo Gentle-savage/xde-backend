dbaa60ad675a06aef1568e0bda602905daaa3fa3
Gentle-savage <gentle,s.emmanuel@gmail.com>
2026-02-12 00:13:30 +0100

    added mongodb connection

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.json());
+
+// CONNECT TO MONGODB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("XDE Logistics API Running Successfully");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server is running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
