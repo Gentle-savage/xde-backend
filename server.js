c490f59f0701fd12ad9171520f57764218af374d
Gentle-savage <gentle,s.emmanuel@gmail.com>
2026-02-12 12:20:43 +0100

    full timeline and status system

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
@@ -19,7 +18,7 @@ function generateTrackingNumber() {
   return "XDE" + Math.floor(100000000 + Math.random() * 900000000);
 }
 
-// Create Shipment (Admin Use)
+// Create Shipment
 app.post("/create-shipment", async (req, res) => {
   try {
     const shipment = new Shipment({
@@ -40,7 +39,34 @@ app.post("/create-shipment", async (req, res) => {
   }
 });
 
-// Track Shipment (Customer Use)
+// Update Shipment Status (Admin Use Later)
+app.post("/update-status/:trackingNumber", async (req, res) => {
+  try {
+    const shipment = await Shipment.findOne({
+      trackingNumber: req.params.trackingNumber,
+    });
+
+    if (!shipment) {
+      return res.status(404).json({ message: "Shipment not found" });
+    }
+
+    shipment.status = req.body.status;
+    shipment.currentLocation = req.body.currentLocation;
+
+    shipment.history.push({
+      location: req.body.currentLocation,
+      message: req.body.message,
+    });
+
+    await shipment.save();
+
+    res.json(shipment);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Track Shipment
 app.get("/track/:trackingNumber", async (req, res) => {
   try {
     const shipment = await Shipment.findOne({
